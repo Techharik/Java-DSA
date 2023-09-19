632cc199489521ac53cd84af242be6b4b77dde0c
Techharik <khariprasath30@gmail.com>
2023-09-19 09:07:23 +0530

    primitive datatypes

---
+++ src/primitive.java
@@ -0,0 +1,14 @@
+public class primitive {
+    public static void main(String[] args)
+
+//       primitive datatypes which is cannont be break into further datatypes
+//        int rollno =45;
+//        char letter ='h';
+//        float marks =78.9f;
+//        double largedecimalvalues =723228.92424242342;
+//        long largeintValues = 32432362432647L;
+//        boolean check = True;
+
+
+    }
+}
