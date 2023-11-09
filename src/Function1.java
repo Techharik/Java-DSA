9cd9ecdc7442bcbeb038d5530ae5785f127e9774
Techharik <khariprasath30@gmail.com>
2023-11-09 15:36:39 +0530

    scopes and varagrs

--- src/Function1.java
+++ src/Function1.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
 
 public class Function1 {
+//  static   int num1 = 0;
     public static void main(String[] args) {
-        int ans = sum();
-        System.out.println(ans);
-
-    }
-    static int sum(){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number 1: ");
         int num1 = in.nextInt();
         System.out.print("Enter number 2: ");
         int num2 = in.nextInt();
 
-        int sum = num1 + num2;
+        int ans = sum(num1, num2);
+        System.out.println(ans);
+        {
+//            int num1
+        }
 
+    }
+
+//    static int sum(){
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter number 1: ");
+//        int num1 = in.nextInt();
+//        System.out.print("Enter number 2: ");
+//        int num2 = in.nextInt();
+//
+//        int sum = num1 + num2;
+//
+//        return sum;
+//    }
+
+
+    //arguments and paramerters:
+    static int sum(int a , int b){
+//        num1 = 10;
+//        System.out.println(num1);
+        int sum = a + b;
         return sum;
     }
 }
